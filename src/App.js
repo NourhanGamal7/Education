b186f7acb5997bc63c5a5d7fff8eea818b2225d7
NourhanGamal7 <aoo446085@gmail.com>
2023-09-04 21:48:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navbar,
+  Home,
+  About,
+  Teacher,
+  Contact,
+  Courses,
+  Footer,
+} from "./components/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-Poppins bg-Solitude">
+      <Navbar />
+      <Home />
+      <About />
+      <Courses />
+      <Teacher />
+      <Contact />
+      <Footer />
     </div>
   );
 }
